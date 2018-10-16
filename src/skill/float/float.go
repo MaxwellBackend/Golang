b0fdef674272da33b1a551d0358137f2deb5ac02
refine1017 <972842322@qq.com>
2018-10-16 15:56:41 +0800

    优化float相乘技巧

--- src/skill/float/float.go
+++ src/skill/float/float.go
@@ -1,11 +1,17 @@
 package float
 
-import "math"
+import (
+	"github.com/shopspring/decimal"
+)
 
-func FloatMulti(value float64) float64 {
-	return value*100
+func FloatMulti(value float64) int {
+	return int(value*100)
 }
 
-func FloatCeilMulti(value float64) int {
-	return int(math.Ceil(value*100))
+func FloatDecimalMulti(value float64) int {
+	decimalValue := decimal.NewFromFloat(value)
+	decimal100 := decimal.NewFromFloat(100)
+	result, _ := decimalValue.Mul(decimal100).Float64()
+
+	return int(result)
 }
--- src/skill/float/float_test.go
+++ src/skill/float/float_test.go
@@ -10,7 +10,7 @@ func TestFloatMulti(t *testing.T) {
 	fmt.Printf("69.99 * 100 = %v\n", FloatMulti(69.99))
 }
 
-func TestFloatCeilMulti(t *testing.T) {
-	fmt.Printf("19.99 * 100 = %v\n", FloatCeilMulti(19.99))
-	fmt.Printf("69.99 * 100 = %v\n", FloatCeilMulti(69.99))
+func TestFloatDecimalMulti(t *testing.T) {
+	fmt.Printf("19.99 * 100 = %v\n", FloatDecimalMulti(19.99))
+	fmt.Printf("69.99 * 100 = %v\n", FloatDecimalMulti(69.99))
 }
